djanowski/facebooker

commit cd7093bab0c32188e5424c8b91d3a9b6741e6ab5

Making other models respond to #id.
Signed-off-by: Mike Mangino <mmangino@elevatedrails.com>

lib/facebooker/session.rb
@@ -537,7 +537,7 @@ module Facebooker
       end
       
       def uid?
-        ! @uid.nil?
+        !! uid
       end
       
       def signature_for(params)

lib/facebooker/model.rb
@@ -77,6 +77,18 @@ module Facebooker
           end)
         end
       end      
+
+      def id_is(attribute)
+        class_eval <<-EOS
+        def #{attribute}=(value)
+          @#{attribute} = value.to_i
+        end
+
+        attr_reader #{attribute.inspect}
+        alias :id #{attribute.inspect}
+        alias :id= #{"#{attribute}=".to_sym.inspect}
+        EOS
+      end
     end
     
     ##

lib/facebooker/models/event.rb
@@ -22,5 +22,7 @@ module Facebooker
     
     include Model
     attr_accessor :eid, :pic, :pic_small, :pic_big, :name, :creator, :update_time, :description, :tagline, :venue, :host, :event_type, :nid, :location, :end_time, :start_time, :event_subtype
+
+    id_is :eid
   end
 end

lib/facebooker/models/friend_list.rb
@@ -6,6 +6,8 @@ module Facebooker
     include Model
     attr_accessor :flid, :name
     
+    id_is :flid
+
     # We need this to be an integer, so do the conversion
     def flid=(f)
       @flid= ( f.nil? ? nil : f.to_i)

lib/facebooker/models/group.rb
@@ -10,6 +10,7 @@ module Facebooker
     include Model
     attr_accessor :pic, :pic_small, :pic_big, :name, :creator, :recent_news, :gid, :update_time, :group_subtype, :group_type, :website, :office, :description, :venue, :nid
     
+    id_is :gid
     
     ##
     # Get the full list of members as populated User objects.  First time fetches group members via Facebook API call.  

lib/facebooker/models/photo.rb
@@ -6,5 +6,7 @@ module Facebooker
                   :link, :caption, :created,
                   :src, :src_big, :src_small,
                   :story_fbid
+
+    id_is :pid
   end
 end

lib/facebooker/models/user.rb
@@ -38,17 +38,8 @@ module Facebooker
       end     
     end
 
-    def uid
-      @uid
-    end
-
-    def uid=(uid)
-      @uid = uid.to_i
-    end
-
-    alias :id :uid
-    alias :id= :uid=
-    alias :facebook_id :uid
+    id_is :uid
+    alias :facebook_id :id
 
     # Returns a user's events, params correspond to API call parameters (except UID):
     # http://wiki.developers.facebook.com/index.php/Events.get

test/facebooker_test.rb
@@ -224,6 +224,7 @@ class TestFacebooker < Test::Unit::TestCase
     photos = @session.get_photos([97503428461115590, 97503428461115573])
     assert_equal 2, photos.size
     assert_equal "Rooftop barbecues make me act funny", photos.first.caption
+    assert_equal 97503428461115590, photos[0].id
   end
   
   def test_can_get_photos_by_subject_and_album

commit ed2467c16ff44c1aaea660362e2f01215a084f59

Fixed a regression after cd7093bab0c32188e5424c8b91d3a9b6741e6ab5.

lib/facebooker/session.rb
@@ -537,7 +537,7 @@ module Facebooker
       end
       
       def uid?
-        !! uid
+        !! @uid
       end
       
       def signature_for(params)